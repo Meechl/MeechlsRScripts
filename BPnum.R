84a44c1e92706f84ff6e25e48b0cbb149da867c8
Meechl <Meechl@users.noreply.github.com>
2015-04-10 14:35:38 -0700

    MS2Rscripts
    This function is called in MS2.R but I don't think I defined it anywhere.

---
+++ BPnum.R
@@ -0,0 +1,13 @@
+BPnum = function( base = "A", sequence = c("AUCG","AUCG")){
+  y = c(0,0,0)
+  for (i in 1:length(sequence)){
+    n = 0
+    for (j in 1:nchar(sequence[i])){
+      if ( substr(sequence[i],j,j) == base ){
+        n = n+1
+      }
+    }
+    y[i] = n
+  }
+  return(y)
+}
